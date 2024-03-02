ab24751b7c31a1aaa2b9e5adc58d7d5ddcc66d0c
hyzmael88 <bartttt15@hotmail.com>
2024-03-01 19:12:37 -0600

    Delete unused 3D models and update file paths in Island.jsx

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -9,7 +9,7 @@ Title: Fox's islands
 import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import islandScene from "../assets/3d/island.glb";
+const islandScene = "/3d/island.glb";
 
 import { a } from "@react-spring/three";
 //a para la animacion
@@ -17,7 +17,7 @@ import { a } from "@react-spring/three";
 const Island = (props) => {
   const islandRef = useRef();
 
-  const { nodes, materials } = useGLTF("/island.glb");
+  const { nodes, materials } = useGLTF(islandScene);
 
   return (
     
