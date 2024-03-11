4aac571e8a248efbad9dccfc869d60c0d8a8a9dc
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-11 17:54:25 -0600

    Refactor state variable names and adjust plane size for different screen sizes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
         POPUP
     </div> */}
 
-    const [rotating, setRotating] = useState(false)
+    const [isRotating, setIsRotating] = useState(false)
 
     const adjustIslandForScreenSize = () =>{
       let screenScale = null
@@ -28,12 +28,31 @@ function Home() {
     }
     return [screenScale, screenPosition, rotation]
   }
+
+    const adjustPlaneForScreenSize = () =>{
+      let screenScale , screenPosition
+
+       if (typeof window !== 'undefined' && window.innerWidth < 768) {
+        screenScale = [1.5,1.5,1.5]
+        screenPosition = [0,-1.5,0]
+    }
+    else{
+      screenScale = [3,3,3]
+      screenPosition=[0,-4,-4]
+    }
+    return [screenScale, screenPosition]
+  }
+
+
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
+  
+  const [planeScale, planePosition] = adjustPlaneForScreenSize()
+
   return (
     <section
     className='w-full h-screen relative'
     >
-        <Canvas className='w-full h-screen bg-transparent'
+        <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{near:0.1, far:1000}}
         >
             <Suspense fallback={<Loader/>}>
@@ -50,8 +69,16 @@ function Home() {
                 position={islandPosition}
                 scale={islandScale}
                 rotation = {islandRotation}
+                isRotating={isRotating}
+                setIsRotating={setIsRotating}
+
                 />
-                <Plane/>
+                <Plane
+                isRotating={isRotating}
+                rotation={[0,20,0]}
+                planeScale={planeScale}
+                planePosition={planePosition}
+                />
                 </Suspense>
 
             </Canvas>
