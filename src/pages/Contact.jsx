22fa655cb857b196ac91f0f2f306cb01d9c52c4f
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-12 00:58:10 -0600

    Add emailjs library and contact form functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,126 @@
-import React from 'react'
+import React, { Suspense, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber'
+import Fox from '@/models/Fox'
+import Loader from '@/components/Loader'
 
 function Contact() {
+  const formRef = useRef(null)
+  const [form, setForm] = useState({
+    name: '', email:'', message:''
+  })
+
+  const [isLoading , setisLoading ] = useState(false)
+  const [currentAnimation, setCurrentAnimation] = useState('idle')
+
+  const handleChange = (e) =>{
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+  const handleSubmit = () =>{
+    e.preventDefault()
+    setisLoading(true)
+    setCurrentAnimation('hit')
+    emailjs.sendForm
+  }
+  const handleFocus = () =>setCurrentAnimation('walk')
+  const handleBlur = () =>setCurrentAnimation('idle')
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-2-[50%] flex flex-col'>
+        <h1 className='head-text'>
+          Get in touch
+        </h1>
+        <form
+        className='w-full flex flex-col gap-7'
+        onSubmit={handleSubmit}
+        >
+          <label className='text-black-500 font-semibold'>
+            Name
+          <input 
+          type='text'	
+          name='name'
+          className='input'
+          placeholder='John'
+          required
+          value={form.name}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          
+          />
+            </label>
+          <label className='text-black-500 font-semibold'>
+            Name
+          <input 
+          type='email'	
+          name='email'
+          className='input'
+          placeholder='Jaizmorateam@gmail.com'
+          required
+          value={form.email}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          
+          />
+            </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+          <textarea
+          name='message'
+          className='textarea'
+          rows={4}
+          placeholder='Let me know how I can help you'
+          required
+          value={form.message}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          
+          />
+            </label>
+            <button 
+            type='submit'
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            >
+              	{
+                  isLoading ? 'Sending...' : 'Send Message'
+                }
+            </button>
+        </form>
+      </div>
+        <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[305px] '>
+                <Canvas
+                camera={
+                  {
+                    position:[0,0,5],
+                    fov:75,
+                    near: 0.1,
+                    far: 1000
+                  
+                  }
+                }
+                >
+                  <directionalLight intensity={2.5}
+                  position={[0,0,1]}
+                  />
+                  <ambientLight intensity={0.5}/>
+                <Suspense fallback={<Loader/>}>
+                  <Fox
+                  currentAnimation={currentAnimation}
+                  position={[0.5,0.35,0]}
+                  rotation={[12.6,-0.6,0]}
+                  scale={[0.5,0.5,0.5]}
+                  />
+                  </Suspense>
+
+                </Canvas>
+        </div>
+    </section>
   )
 }
 
