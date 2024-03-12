f8553506b139c49468e675a3a7fd7a726115b0b9
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-11 21:54:01 -0600

    Rotate island based on rotation speed

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -117,6 +117,9 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
       if (Math.abs(rotationSpeed.current) < 0.001) {
         rotationSpeed.current = 0;
       }
+
+      islandRef.current.rotation.y += rotationSpeed.current;
+
     } else {
       const rotation = islandRef.current.rotation.y;
 
